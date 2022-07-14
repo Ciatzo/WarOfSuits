1b1a86b7019a664bbd1872870fb4a587d6f6f915
Cian Jansen <ciatzo2@gmail.com>
2022-07-14 15:55:19 +0200

    Remove redundant dagger package

--- app/src/main/java/com/cianjansen/warofsuits/application/WarOfSuitsApplication.kt
+++ app/src/main/java/com/cianjansen/warofsuits/application/WarOfSuitsApplication.kt
@@ -1,9 +1,5 @@
 package com.cianjansen.warofsuits.application
 
 import android.app.Application
-import com.cianjansen.warofsuits.dagger.DaggerApplicationComponent
 
-class WarOfSuitsApplication : Application() {
-
-    val wosComponent = DaggerApplicationComponent.create()
-}
+class WarOfSuitsApplication : Application()
